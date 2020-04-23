c145538b2e25de20ea544f41bc22463d05ff165d
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-04-23 14:29:09 +1200

    PRODUCTION: Added hostname

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 8080;
 const app = require('./app');
 
 const server = http.createServer(app);
-server.listen(port, function() {
+server.listen(port, '172.31.89.21',function() {
     console.log(`Listening on port ${port}`);
 });
