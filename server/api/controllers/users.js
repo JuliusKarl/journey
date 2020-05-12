bce5faeca9c6f3163496be44baee010474ea364c
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-12 18:35:56 +1200

    LOGIN: Non-existent credential handler

--- server/api/controllers/users.js
+++ server/api/controllers/users.js
@@ -100,7 +100,7 @@ exports.user_login = (req, res, next) => {
                     }, process.env.JWT_KEY, {
                         expiresIn: "1h"
                     });
-                    res.redirect('http://localhost:3000/');
+                    res.redirect('http://18.233.138.219');
                 }
             })
         })
