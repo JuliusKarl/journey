df9d67f8888eacdaf608afe12ba4fbae37491722
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-12 18:34:15 +1200

    LOGIN: Non-existent credential handler

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UsersController = require("../controllers/users");
-const checkAuth = require("../middleware/check-auth");
+// const checkAuth = require("../middleware/check-auth");
 
 // Get all existing users
-router.get('/', checkAuth, UsersController.user_get_all);
+router.get('/', UsersController.user_get_all);
 
 // Create a new user
 router.post('/signup' , UsersController.user_post_one);
@@ -16,7 +16,7 @@ router.post('/check_email' , UsersController.user_check_email);
 router.post('/login', UsersController.user_login);
 
 // Find an existing user by userID
-router.get('/find', checkAuth, UsersController.user_find_one)
+router.get('/find', UsersController.user_find_one)
 
 // Delete an existing user by userId
 router.delete('/:userId', UsersController.user_delete_one);
