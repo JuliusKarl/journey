efaeb155f5dcc47ba0f8c342176f6c956bec3dc4
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-15 04:00:28 +1200

    CSS: Common font and standard text

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -64,7 +64,7 @@ export default class App extends Component {
                   <Button 
                     className="button"
                     href="/login" 
-                    variant="outline-light">Login
+                    variant="link">Login
                   </Button>
                 </Nav> 
               </Navbar.Collapse> 
