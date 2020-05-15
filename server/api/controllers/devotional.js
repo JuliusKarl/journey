14e215a8edef60cc6a30ee8373ee52b610d8def7
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-16 01:37:02 +1200

    LOGO: Updated logo and favicon

--- server/api/controllers/devotional.js
+++ server/api/controllers/devotional.js
@@ -1,13 +1,11 @@
 const Devotional = require("../models/devotional");
 const fetch = require('node-fetch');
 
-// Get single product
+/** Get single daily devotional */
 exports.devotional_get_one = (req, res, next) => {
     fetch('https://devotionalium.com/api/v2')
         .then(response => response.json())
         .then(response => res.status(200).json(response))
         .catch(err => {
-            res.status(500).json({error: err})
-        });
-}
+            res.status(500).json({error: err})});}
 
