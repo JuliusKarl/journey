889b9c79dfb35d21ad45b4e110582671abb7b19a
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-18 21:42:42 +1200

    check email exists

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -95,6 +95,7 @@ exports.user_post_one = (req, res, next) => {
                                 return res.status(200).json({
                                     message: "Auth Success",
                                     status: true})})
+                            .then(() => console.log(user))
                             .catch(err => {
                                 res.status(500).json({
                                     error: err});});}});}})}
