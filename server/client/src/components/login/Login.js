71ae97cb901a9df3c551cfaa3dbc614d8ff26025
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-18 22:33:13 +1200

    added header

--- server/client/src/components/login/Login.js
+++ server/client/src/components/login/Login.js
@@ -71,7 +71,7 @@ class Login extends Component {
         e.preventDefault();
         fetch('/user/signup', {
                 method: 'POST',
-                headers : { 'Content-Type': 'application/json' },
+                headers : { 'Content-Type': 'application/json', 'Accept': 'application/json'},
                 body: JSON.stringify({
                     name : this.state.name,
                     email : this.state.email,
